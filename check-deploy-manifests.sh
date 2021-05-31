b71929e4020cc77635717caa701595b36e759e84
Fabian Martinez <famartin@redhat.com>
2021-05-31 17:42:22 +0200

    include deployment manifests

---
+++ check-deploy-manifests.sh
@@ -0,0 +1,18 @@
+diff dist/kubernetes/standalone/target/kubernetes/manifests/ deploy/standalone/
+ret=$?
+
+if [[ $ret -eq 0 ]]; then
+    echo "Standalone manifests ok."
+else
+    echo "Standalone manifests are not the expected ones."
+    exit 1
+fi
+
+diff dist/kubernetes/simple/target/kubernetes/manifests/ deploy/simple/
+ret=$?
+if [[ $ret -eq 0 ]]; then
+    echo "Simple manifests ok."
+else
+    echo "Simple manifests are not the expected ones."
+    exit 1
+fi
---
+++ copy-deploy-manifests.sh
@@ -0,0 +1,2 @@
+cp dist/kubernetes/standalone/target/kubernetes/manifests/*.yaml deploy/standalone/
+cp dist/kubernetes/simple/target/kubernetes/manifests/*.yaml deploy/simple
